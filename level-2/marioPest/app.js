43ae920f6c2605a060fee06d0f31a422fa0f13ad
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-09-27 17:01:27 -0600

    initial commit

--- level-2/marioPest/app.js
+++ level-2/marioPest/app.js
@@ -10,53 +10,187 @@ function myFunction() {
 
 const form = document.marioPest
 
+
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let numOfGoomba = form.numOfGoomba.value
+    const goombaPrice = Number(numOfGoomba) * 5
+    let numofBomb = form.numOfBomb.value
+    const bombPrice = Number(numofBomb) * 7
+    let numOfCheep = form.numOfCheep.value
+    const cheapPrice = Number(numOfCheep) * 11
+
+
+    const combinedTotal = goombaPrice + bombPrice + cheapPrice
+    const li = document.getElementById("allResult")
+    li.textContent = " ";
+
+    li.append(combinedTotal)
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
 // Goomba Baddie Pricing
 
-const goombaResult = form.querySelector('#goombaBaddie').querySelector('#goombaButton')
-console.log(goombaResult)
+// const goombaResult = form.querySelector('#goombaBaddie').querySelector('#goombaButton')
+// console.log(goombaResult)
 
-goombaResult.addEventListener('click', function(event) {
+// goombaResult.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     let numofGoomba = form.numOfGoomba.value
+//         // let priceofGoomba = form.priceOfGoomba.value
+//     const goombaPrice = form.numOfGoomba * 5
+//     let li = document.getElementById("goombaResult");
+//     console.log(li)
+//     li.textContent = " ";
+//     console.log(goombaPrice);
+//     li.append(goombaPrice)
+// })
+
+
+
+
+
+
+
+
+// // bombb pricing
+
+// const bombResult = form.querySelector('#bombBaddie').querySelector('#bombButton')
+// console.log(bombResult)
+
+// bombResult.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     let numofBomb = form.numOfBomb.value
+//         // let priceOfBomb = form.priceOfBomb.value
+//     const bombPrice = Number(numofBomb) * 7
+//     let li = document.getElementById("bombResult");
+//     console.log(li)
+//     li.textContent = " ";
+//     console.log(bombPrice);
+//     li.append(bombPrice)
+// })
+
+// // Cheep Cheep Pricing
+// const cheapResult = form.querySelector('#cheepBaddie').querySelector('#cheepButton')
+// console.log(cheapResult)
+
+// cheapResult.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         const numOfCheep = form.numOfCheep.value
+//             // let priceOfCheep = form.priceOfCheep.value
+//         const cheapPrice = Number(numOfCheep) * 11
+//         const li = document.getElementById("cheepResult");
+//         console.log(li)
+//         li.textContent = " ";
+//         console.log(cheapPrice);
+//         li.append(cheapPrice)
+//     })
+//     // Total Price for all the baddies
+
+
+
+// form.addEventListener('submit', function(event) {
+//         event.preventDefault();
+//         const cheapPrice = Number(numOfCheep) * 11
+//         const bombPrice = Number(numofBomb) * 7
+//         const goombaPrice = Number(numofGoomba) * 5
+
+
+//         const combinedTotal = cheapPrice + bombPrice + goombaPrice
+
+//         const li = document.getElementById("allResult")
+//         li.textContent = " ";
+//         li.append(combinedTotal)
+
+//     }
+
+
+// )
+
+form.addEventListener('submit', function(event) {
     event.preventDefault();
-    let numofGoomba = form.numOfGoomba.value
-        // let priceofGoomba = form.priceOfGoomba.value
-    let goombaPrice = Number(numofGoomba) * 5
-    let li = document.getElementById("goombaResult");
-    console.log(li)
-    li.textContent = " ";
-    console.log(goombaPrice);
-    li.append(goombaPrice)
-})
-
-// bombb pricing
-
-const bombResult = form.querySelector('#bombBaddie').querySelector('#bombButton')
-console.log(bombResult)
-
-bombResult.addEventListener('click', function(event) {
-    event.preventDefault();
+    let numOfGoomba = form.numOfGoomba.value
+    const goombaPrice = Number(numOfGoomba) * 5
     let numofBomb = form.numOfBomb.value
-        // let priceOfBomb = form.priceOfBomb.value
-    let bombPrice = Number(numofBomb) * 7
-    let li = document.getElementById("bombResult");
-    console.log(li)
+    const bombPrice = Number(numofBomb) * 7
+    let numOfCheep = form.numOfCheep.value
+    const cheapPrice = Number(numOfCheep) * 11
+
+
+    const combinedTotal = goombaPrice + bombPrice + cheapPrice
+    const li = document.getElementById("allResult")
     li.textContent = " ";
-    console.log(bombPrice);
-    li.append(bombPrice)
+
+    li.append(combinedTotal)
+
+
+
+
+
 })
 
-// Cheep Cheep Pricing
-const cheapResult = form.querySelector('#cheepBaddie').querySelector('#cheepButton')
-console.log(cheapResult)
 
-cheapResult.addEventListener('click', function(event) {
-        event.preventDefault();
-        let numOfCheep = form.numOfCheep.value
-            // let priceOfCheep = form.priceOfCheep.value
-        let cheapPrice = Number(numOfCheep) * 11
-        let li = document.getElementById("cheepResult");
-        console.log(li)
-        li.textContent = " ";
-        console.log(cheapPrice);
-        li.append(cheapPrice)
-    })
-    // Total Price for all the baddies
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const totalPrice = cheapPrice + bombPrice + goombaPrice
+// console.log(totalPrice);
+
+
+// const allTotal = form.querySelector('#allbaddiettotal')
+
+
+// allTotal.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         let li = document.getElementById("allResult");
+//         console.log(li)
+//         li.textContent = " ";
+//         console.log(totalPrice);
+//         li.append(totalPrice)
+//     }
+
+
+
+
+
+
+
+// function toalCost(event) {
+//     allbaddietotal.addEventListener('click', function(event) {
+//             event.preventDefault();
+
+
+//             let li = document.getElementById("allResult");
+//             let baddiethree = cheapPrice + bombPrice + goombaPrice
+//             console.log(baddiethree);
+//             li.append(baddiethree);
+//             toalCost();
+//         }
+
+//     )
