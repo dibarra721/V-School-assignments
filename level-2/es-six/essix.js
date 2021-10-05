34ab985c474b7175c7275cb2672b5757b9b1680c
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-10-05 12:39:49 -0600

    initial commit

--- level-2/es-six/essix.js
+++ level-2/es-six/essix.js
@@ -1,72 +1,64 @@
-
-// Replace all the vars with 
+// Replace all the vars with
 // let and const. Alter the code however necessary to make sure this
 //  continues to work (so the pet's name isn't "John", but instead "spot" is returned). You only need to delete var and insert let and const
 
-// John is the pet owner, and 
+// John is the pet owner, and
 // his name should be stored differently than the other names.
 
-
-let name = "John"
-const age = 101
+let name = "John";
+const age = 101;
 
 function runForLoop(pets) {
-    var petObjects = []
-    for (var i = 0; i < pets.length; i++) {
-        var pet = { type: pets[i] }
-        let name;
-        if (pets[i] === "cat") {
-            name = "fluffy"
-        } else {
-            name = "spot"
-        }
-        console.log("pet name: ", name)
-        pet.name = name
-        petObjects.push(pet)
+  var petObjects = [];
+  for (var i = 0; i < pets.length; i++) {
+    var pet = { type: pets[i] };
+    let name;
+    if (pets[i] === "cat") {
+      name = "fluffy";
+    } else {
+      name = "spot";
     }
-    console.log("man name", name)
-    // man.name= name
-    return petObjects
+    console.log("pet name: ", name);
+    pet.name = name;
+    petObjects.push(pet);
+  }
+  console.log("man name", name);
+  // man.name= name
+  return petObjects;
 }
 
-runForLoop(["cat", "dog"])
-
-
+runForLoop(["cat", "dog"]);
 
 // Re-write this .map() using an arrow function:
 
 // Be aware that if JavaScript sees a { directly after the => it will think it's starting a function, and not starting an object, so the : will be an unexpected symbol.
-const carrots = ["bright orange", "ripe", "rotten"]
+const carrots = ["bright orange", "ripe", "rotten"];
 
-
-const mapVegetables = arr => {
-    return arr.map(function(carrot) {
-        return { type: "carrot", name: carrot }
-    })
-}
+const mapVegetables = (arr) => {
+  return arr.map(function (carrot) {
+    return { type: "carrot", name: carrot };
+  });
+};
 console.log(mapVegetables(carrots));
 
-
-
-
 const people = [
-    {
-        name: "Princess Peach",
-        friendly: false
-    },
-    {
-        name: "Luigi",
-        friendly: true
-    },
-    {
-        name: "Mario",
-        friendly: true
-    },
-    {
-        name: "Bowser",
-        friendly: false
-    }
-]
+  {
+    name: "Princess Peach",
+    friendly: false,
+  },
+  {
+    name: "Luigi",
+    friendly: true,
+  },
+  {
+    name: "Mario",
+    friendly: true,
+  },
+  {
+    name: "Bowser",
+    friendly: false,
+  },
+];
 
 // function filterForFriendly(arr) {
 //     return arr.filter(function(person) {
@@ -74,16 +66,13 @@ const people = [
 //     })
 // }
 
-const filterForFriendly = arr => {
-    return arr.filter(function(person){
-    return person.friendly
-
-})
-
-}
-console.log(filterForFriendly(people))
+const filterForFriendly = (arr) => {
+  return arr.filter(function (person) {
+    return person.friendly;
+  });
+};
+console.log(filterForFriendly(people));
 // Re-write the following functions to be arrow functions:
-
 
 // function doMathSum(a, b) {
 //     return a + b
@@ -95,44 +84,44 @@ console.log(filterForFriendly(people))
 
 const doMathSum = (a, b) => a + b;
 
-const produceProduct = (a,b) => a * b;
+const produceProduct = (a, b) => a * b;
 
-console.log(doMathSum(5,3))
+console.log(doMathSum(5, 3));
 
-console.log(produceProduct(4, 5))
+console.log(produceProduct(4, 5));
 
 // Task 4
-// Write a printString function that takes firstName, 
+// Write a printString function that takes firstName,
 // lastName, and age as parameters and returns a string like the following:
 
 // firstName should default to "Jane"
 // lastName should default to "Doe"
 // age should default to 100
 
-const printString = (firstName = 'Jane', lastName = 'Doe', age= 100) => `Hi ${firstName} ${lastName}, how does it feel to be ${age}`
+const printString = (firstName = "Jane", lastName = "Doe", age = 100) =>
+  `Hi ${firstName} ${lastName}, how does it feel to be ${age}`;
 console.log(printString());
 // Task 5
 // Use the shorthand syntax to make the following filter take up one line. Copy and paste the array to test it.
 
-
 const animals = [
-    {
-        type: "dog",
-        name: "theodore"
-    },
-    {
-        type: "cat",
-        name: "whiskers"
-    },
-    {
-        type: "pig",
-        name: "piglette"
-    },
-    {
-        type: "dog",
-        name: "sparky"
-    }
- ];
+  {
+    type: "dog",
+    name: "theodore",
+  },
+  {
+    type: "cat",
+    name: "whiskers",
+  },
+  {
+    type: "pig",
+    name: "piglette",
+  },
+  {
+    type: "dog",
+    name: "sparky",
+  },
+];
 
 //  function filterForDogs(arr) {
 //     return arr.filter(animal => {
@@ -144,14 +133,16 @@ const animals = [
 //     })
 // }
 
-const filterForDogs = arr => arr.filter(animal => (animal.type === "dog" ? true : false))
+const filterForDogs = (arr) =>
+  arr.filter((animal) => (animal.type === "dog" ? true : false));
 console.log(filterForDogs(animals));
 
 //  Using template literals, write a function that takes location and name parameters and outputs a message formatted like this:
 
 // Hi Janice!
-// Welcome to Hawaii. 
-// I hope you enjoy your stay. Please ask the president of Hawaii if you need anything. 
+// Welcome to Hawaii.
+// I hope you enjoy your stay. Please ask the president of Hawaii if you need anything.
 
-const greeting= (locationTravel= 'Hawaii', firstName='Janice') => `Hi ${firstName}! \nWelcome to ${locationTravel}. \nI hope you enjoy your time here `
+const greeting = (locationTravel = "Hawaii", firstName = "Janice") =>
+  `Hi ${firstName}! \nWelcome to ${locationTravel}. \nI hope you enjoy your time here `;
 console.log(greeting());
--- level-2/es-six/princess.js
+++ level-2/es-six/princess.js
@@ -1,111 +1,85 @@
-class Player{
-    constructor(name, totalCoins = 0, hasStar){
+class Player {
+  constructor(name, totalCoins = 0, hasStar) {
     this.name = name;
-    this.totalCoins= totalCoins;
-    this.hasStar= hasStar;
-    this.statusOption= "Powered Up";
+    this.totalCoins = totalCoins;
+    this.hasStar = hasStar;
+    this.statusOption = "Powered Up";
+  }
 
-}
-// setName(){
-//     const namePicked= Math.floor(Math.random() * 100)
-//     if (namePicked > 50){
-//         return this.name ='Mario'
-        
-//     } else if (namePicked < 50 ){
-//        return this.name ='Lugi'
-//     }
-// }
-setName(namePicked){
-    if(namePicked === `Mario` || namePicked === `Luigi`) {
-        return this.name = namePicked
+  setName(namePicked) {
+    if (namePicked === `Mario` || namePicked === `Luigi`) {
+      return (this.name = namePicked);
     }
-}
+  }
 
+  gotHit() {
+    const status = ["Powered Up", "Big", "Small", `Dead`];
+    let prevIndex = status.findIndex((status) => status === this.statusOption);
+    if (prevIndex < 3) {
+      this.statusOption = status[prevIndex + 1];
 
-gotHit(){
-
-    const status= ["Powered Up", "Big", "Small", `Dead`]
-  let prevIndex = status.findIndex((status) => status === this.statusOption)
-      if(prevIndex < 3){
-          this.statusOption = status[prevIndex + 1 ]
-        
-          if(prevIndex === 0)    {
-              this.hasStar = false;
-          }
-
-      } else {
-          this.statusOption =  status[prevIndex]
+      if (prevIndex === 0) {
+        this.hasStar = false;
       }
-
-}
-
-gotPoweredUp(){
-    const status= ["Powered Up", "Big", "Small", `Dead`]
-
-    let prevIndex = status.findIndex((status) => status === this.statusOption)
-        if (prevIndex > 0) {
-            this.statusOption = status[prevIndex -1]
-            
-        } else {
-            this.statusOption =status[prevIndex]
-            this.hasStar = true;
-
-        }
-    
-
-  
-}
-
-addCoins(){
-    this.totalCoins++
-}
-
-print(){
-
-    console.log(`Name: ${this.name}`)
-    console.log(`Total Coins:${this.totalCoins}`)
-    console.log( `Status: ${this.statusOption}`)
-    console.log(`Has Star: ${this.hasStar}`)
-}
-
-
-}
-
-
-const randomRange = function(player){
-    let randomNum = Math.floor(Math.random() * 3)
-    switch (randomNum) {
-        case 0:
-            player.gotHit()
-            break;
-        case 1:
-            player.gotPoweredUp()
-            break;
-        case 2:
-            player.addCoins()
-            break;
-        default:
-            break;
+    } else {
+      this.statusOption = status[prevIndex];
     }
-}  
+  }
 
-let gameActive = true
+  gotPoweredUp() {
+    const status = ["Powered Up", "Big", "Small", `Dead`];
 
-const character= new Player('Lugi', 0 , true)
+    let prevIndex = status.findIndex((status) => status === this.statusOption);
+    if (prevIndex > 0) {
+      this.statusOption = status[prevIndex - 1];
+    } else {
+      this.statusOption = status[prevIndex];
+      this.hasStar = true;
+    }
+  }
+
+  addCoins() {
+    this.totalCoins++;
+  }
+
+  print() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Total Coins:${this.totalCoins}`);
+    console.log(`Status: ${this.statusOption}`);
+    console.log(`Has Star: ${this.hasStar}`);
+  }
+}
+
+const randomRange = function (player) {
+  let randomNum = Math.floor(Math.random() * 3);
+  switch (randomNum) {
+    case 0:
+      player.gotHit();
+      break;
+    case 1:
+      player.gotPoweredUp();
+      break;
+    case 2:
+      player.addCoins();
+      break;
+    default:
+      break;
+  }
+};
+
+let gameActive = true;
+
+const character = new Player("Lugi", 0, true);
 
 const startGame = setInterval(() => {
-    if(gameActive){
-        randomRange(character) ;
-       character.print();
+  if (gameActive) {
+    randomRange(character);
+    character.print();
 
-        if(character.statusOption ===`Dead`){
-            gameActive = false 
-        }
-    } else {
-        clearInterval(startGame)
+    if (character.statusOption === `Dead`) {
+      gameActive = false;
     }
-
+  } else {
+    clearInterval(startGame);
+  }
 }, 1000);
-
-
-
