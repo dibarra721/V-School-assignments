1b8f26f66c7ba25bca6262ca95df66130980e3c3
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-10-12 10:31:01 -0600

    capstone done

--- level-2/todo-list/app.js
+++ level-2/todo-list/app.js
@@ -1,53 +1,47 @@
 function getData() {
   axios
     .get("https://api.vschool.io/dianaibarra/todo")
-    .then((res) => listData(res.data))
-    .catch((err) => console.log(err));
+    .then(res => listData(res.data))
+    .catch(err => console.log(err))
 }
 
-function clearList() {
-  const el = document.getElementById("myList");
-  while (el.firstChild) {
-    el.removeChild(el.firstChild);
-  }
-}
 
-const newLi = document.createElement("li");
+getData()
 
 function listData(data) {
-  clearList();
+  clearList()
 
+const newLi = document.createElement("li");
   document.getElementById("myList").appendChild(newLi);
 
   for (let i = 0; i < data.length; i++) {
     // show list title
     const h1 = document.createElement("h1");
-    h1.textContent = `Title: ${data[i].title}`;
+    h1.textContent = ` Title:  ${data[i].title}`;
     h1.style.fontFamily= "'Lobster', cursive";
-    h1.style.color="#222239";
+    h1.style.color="#ffffff";
     h1.style.textAlign="center";
     newLi.appendChild(h1);
     // price
     const price = document.createElement("price");
-    price.textContent = `Price: ${data[i].price}`;
+    price.textContent = ` Price:  ${data[i].price}`;
     newLi.appendChild(price);
 
     // description
     const des = document.createElement("des");
-    des.textContent = `Description: ${data[i].description}`;
+    des.textContent = ` Description:  ${data[i].description} `;
     newLi.appendChild(des);
 
     // image
     const imageEl = document.createElement("img");
     imageEl.src = data[i].imgUrl;
-    imageEl.style.width = "200px";
-    imageEl.style.height = "250px";
+    imageEl.style.width = "100px";
+    imageEl.style.height = "150px";
     newLi.appendChild(imageEl);
 
     // check box and label
     const checkBox = document.createElement('input');
     checkBox.setAttribute("type", "checkbox");
-    // checkBox.checked = data.completed;
     checkBox.style.height = "20px";
     checkBox.style.width = "20px";
     checkBox.setAttribute("name", "completed");
@@ -71,7 +65,7 @@ function listData(data) {
           .catch((err) => console.log(err));
         h1.style.textDecoration = "line-through";
         h1.style.fontFamily= "monospace"
-        h1.style.color = "blue";
+        h1.style.color = "#e33f84";
       } else {
         axios
           .put(`https://api.vschool.io/dianaibarra/todo/${data[i]._id}`, {
@@ -87,33 +81,38 @@ function listData(data) {
 const deleteButton= document.createElement('button')
 deleteButton.setAttribute('type', 'button')
 deleteButton.setAttribute('id', 'button')
-deleteButton.textContent='Delete'
-deleteButton.style.width= "60px"
-deleteButton.style.backgroundColor="#C4AEA8"
-deleteButton.style.borderRadius= "10px"
+deleteButton.textContent='X'
+deleteButton.style.width= "fit-content"
+deleteButton.style.height="fit-content"
+deleteButton.style.backgroundColor="#e33f84"
+// deleteButton.style.borderRadius= "10px"
 newLi.appendChild(deleteButton)
 
 deleteButton.addEventListener('click', function (event) {
   event.preventDefault()
 
   axios.delete(`https://api.vschool.io/dianaibarra/todo/${data[i]._id}`)
-      .then(res => getData())
+      .then((res) => console.log(res))
       .catch(err => console.log(err))
 })
-
-
 
 
   }
 }
 
+function clearList() {
+  const el = document.getElementById('myList');
+  while (el.firstChild) {
+    el.removeChild(el.firstChild);
+  }
+}
 
-getData();
 
 const todoForm = document["todoform"];
 
 todoForm.addEventListener("submit", function (event) {
   event.preventDefault();
+  
 
   const newTodo = {
     title: todoForm.title.value,
@@ -129,8 +128,8 @@ todoForm.addEventListener("submit", function (event) {
 
   axios
     .post("https://api.vschool.io/dianaibarra/todo", newTodo)
-    .then((res) => getData())
-    .catch((error) => console.log(error));
+    .then(res => getData())
+    .catch(err => console.log(err))
 });
 
 
