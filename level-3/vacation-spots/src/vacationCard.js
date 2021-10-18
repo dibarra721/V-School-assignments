39509986a618b38479745d3c2c0188d748cef521
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-10-18 15:51:12 -0600

    update

--- level-3/vacation-spots/src/vacationCard.js
+++ level-3/vacation-spots/src/vacationCard.js
@@ -1,24 +1,37 @@
 import React from "react"
 import "./styles.css"
 
+function dollar(props){
+    let dollar = "";
+
+    if (props > 1000) {
+        dollar = "$$$";
+    } else if (props < 500) {
+        dollar = "$";
+    } else {
+        dollar = "$$";
+    }
+
+    return dollar;
+}  
+
 
 function VacationCard(props) {
+console.log(props);
 
 
-
-    
     return (
         <div>
             <h2>{props.location.place}</h2>
-            <p>Price: {props.location.price}
-            -Best Time to Visit : {props.location.timeToGo}</p>
+            <p>Price: {dollar(props.location.price)}<br></br>
+            Best Time to Visit : {props.location.timeToGo}</p>
                
         </div>
     )
 
+    }
 
 
-}
 
 
 
