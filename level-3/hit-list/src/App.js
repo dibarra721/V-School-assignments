f3272b3f48e31c12cd136cb25eef7813257fd238
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-10-27 09:17:04 -0600

    code and style update

--- level-3/hit-list/src/App.js
+++ level-3/hit-list/src/App.js
@@ -3,17 +3,15 @@ import "./style.css"
 
  
   class App extends Component {
-    constructor() {
-      super()
-    this.state = {
-      hitList:([])
-    }
+    state = {
+      hitList:[]
+    
 
     }
 
     componentDidMount() {
       fetch("https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json")
-          .then(data => data.json())
+          .then(response => response.json())
           .then(data => {
               this.setState({
                   hitList: data
@@ -42,10 +40,7 @@ console.log(list)
     
    <h1>The hit List</h1>
 
-   <div className="theList">
-{list.name}
-{list.image}
-{this.state.hitList.name}
+   <div className="theList">{list}
     </div>
     </div>
   )
