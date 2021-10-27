7c0ca553f196d61fdb706d6640ea92a8c1a23a4a
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-10-27 11:18:14 -0600

    random color

---
+++ level-3/random-color/src/RandomColor.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+import './App.css';
+import axios from 'axios';
+
+class RandomColor extends Component{
+  state ={
+    color:[{hex:""}]
+  }
+
+  componentDidMount() {
+    this.newColor ()
+  }
+  handleChange = () => {
+    this.newColor ()
+  }
+
+  newColor = () => {
+    axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
+    .then(response => response.data)
+    .then(data => {
+      this.setState({
+        color: data.colors,
+      })
+    })
+  }
+
+  render () {
+
+    return(
+      <div style={{backgroundColor : `#${this.state.color[0].hex}`, height:"600px", display:"block"}}>
+        <button onClick={this.handleChange}>Click to Change Color</button>
+      </div>
+    )
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
+export default RandomColor;
