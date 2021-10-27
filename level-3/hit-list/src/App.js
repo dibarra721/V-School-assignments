6fb6745296209df977766dbf8ad127dac6c40500
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-10-27 09:19:28 -0600

    code update

--- level-3/hit-list/src/App.js
+++ level-3/hit-list/src/App.js
@@ -1,54 +1,42 @@
-import React, {Component} from "react"
-import "./style.css"
+import React, { Component } from "react";
+import "./style.css";
 
- 
-  class App extends Component {
-    state = {
-      hitList:[]
-    
-
-    }
-
-    componentDidMount() {
-      fetch("https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json")
-          .then(response => response.json())
-          .then(data => {
-              this.setState({
-                  hitList: data
-                
-                  })  
-              console.log(data)
-          })
+class HitList extends Component {
+  state = {
+    hitList: []
   }
-    
 
-render() {
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          hitList: data
+        });
+        console.log(data)
+      })
+  }
 
-const list= this.state.hitList.map((name,index) => {
-return (
-  <div className="hitList" key={index}>
-    <p className="name">Name:{name.name}</p>
-    <img className="image"src={name.image} alt=""></img>
-    </div>
-)
+  render() {
+    const list = this.state.hitList.map((name, index) => {
+      return (
+        <div className="hitList" key={index}>
+          <p className="name">Name:{name.name}</p>
+          <img className="image" src={name.image} alt=""></img>
+        </div>
+      )
+    })
+  
+    return (
+      <div>
+        <h1>The hit List</h1>
 
-})
-console.log(list)
-
-  return (
-    <div>
-    
-   <h1>The hit List</h1>
-
-   <div className="theList">{list}
-    </div>
-    </div>
-  )
+        <div className="theList">{list}</div>
+      </div>
+    );
+  }
 }
 
-    
-  }
-
-  
-
-export default App
+export default HitList
